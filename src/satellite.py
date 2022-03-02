239a9e062208c227bf1f59d07de4d51a95b12e1e
Ethan <ethan.hawk@valpo.edu>
2022-03-02 17:24:18 -0600

    satellite.py: add

---
+++ src/satellite.py
@@ -0,0 +1,22 @@
+class Satellite:
+    sat_id = ""
+    normad = 0
+    name = ""
+    names = ""
+    image = ""
+    status = ""
+    is_alive = False
+    is_decayed = False
+    is_launched = False
+    is_deployed = False
+    website = ""
+    operator = ""
+    countries = []
+    telemetries = []
+    updated = ""
+    citation = ""
+    associated_satellites = []
+
+
+    def __init__():
+        assert False, "Satellite.__init__() not implemented yet"

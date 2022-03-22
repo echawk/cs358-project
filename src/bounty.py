78f990aa19e6ca36cdb378e3d1ea1bcd937b5bd8
Ethan <ethan.hawk@valpo.edu>
2022-03-22 11:26:43 -0500

    Bounty: add

---
+++ src/bounty.py
@@ -0,0 +1,8 @@
+class Bounty:
+    """
+    A bounty is effectively a way to have a type of currency that allows for
+    jobs to be made.
+    """
+
+    def __init__(value):
+        self.value: int = value

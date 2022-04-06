631ae513a6e1d9d90da4986bc62d001667d443bb
ErikjPetersen <65267303+ErikjPetersen@users.noreply.github.com>
2022-04-06 11:52:46 -0500

    updated with extra test code

--- src/test1.py
+++ src/test1.py
@@ -25,31 +25,19 @@ def score(INnode, Preferences):
 
   if nodetype == 1: #Sattelite Scoring
       print("This is a satellite")
-<<<<<<< Updated upstream
+
       if INnode["status"] == "dead":
+
           scoreval = scoreval - 100
 
     #check status if alive give x if dead give -1000000
-=======
-      if INnode["status"] == "dead": #if station is dead give -100 score
-        scoreval = scoreval - 100
-      if INnode["country"] == "Russia": #if station is dead give -100 score
-        scoreval = scoreval - 100
 
-
->>>>>>> Stashed changes
     #check countries
     #check pass
 
   elif nodetype == 2: #Station Scoring
     print("This is a station")
-<<<<<<< Updated upstream
-    #check status if alive give x if dead give -1000000
 
-
-  print("The score of this node is ",scoreval)
-
-=======
     if INnode["status"] == "dead": #if station is dead give -100 score
       scoreval = scoreval - 100
 
@@ -57,7 +45,6 @@ def score(INnode, Preferences):
 
   print("The score of this node is ",scoreval)
   return scoreval
->>>>>>> Stashed changes
 
 #NOI = 1#command to take an individual node (sattelite or station) from the list and turn it into Node of interest
 satellite = satellitelist.json()
@@ -66,3 +53,72 @@ NOI = satellite[35] #accesses the first object in the json use sat[0]["name"] fo
 PrefList = 0 #object containing the preferenced used in the scoring function
 score(NOI,PrefList)
 
+
+# FROM OLD SATPASS.py
+# var names
+downlink_low = 0 #low end of the transmitter freq range
+downlink_high = 0 #high end of the transmitter freq range
+station_low = 0 #low end of the station freq range
+station_high = 0 #high end of the station freq range
+sLat = 0 #station Latitude
+sLong = 0 #station Longitude
+
+# Functions
+
+def TrashSatellite():
+    print("satellite is invalid")
+
+def create_observer(lat, lon, alt, min_riseset=0.0):
+    '''
+    Create an observer instance.
+    '''
+    # pylint: disable=assigning-non-slot
+    observer = ephem.Observer()
+    observer.lat = str(lat)
+    observer.lon = str(lon)
+    observer.elevation = alt
+    observer.horizon = str(min_riseset)
+
+    return observer
+
+
+def FindPasses(satellite, observer, tmin, tmax, minimum_altitude, min_pass_duration):
+    passes = []
+
+    sat_ephem = ephem.readtle(str(satellite.tle0))
+
+    #set start time
+    # observer.date = ephem.date(tmin)
+
+
+# Find passes
+#print('Finding all passes for %s satellites:' % len(satellites))
+
+
+
+#loop over all passes
+# is the satellite above the horizon (aka during a pass) at this time?
+# is the transmitter frequency (`downlink_*`) within the frequency range of the antenna(s)?
+
+#LOOP though all of the passes found using the following criteria
+SatTLE = 0 #satellite position prediction
+if downlink_low < station_low:
+    TrashSatellite()
+if downlink_high > station_high:
+    TrashSatellite()
+#check if the sattelite pass is ABOVE the horizon
+
+
+    #TEST FROM SCHEDULING FUNCTION
+    for j in range(0, len(List)):
+        CompSat = List[j]
+        if CompSat.Sperc > 70.0:
+            if SchedNum > 1: #if this is NOT the first sat being scheduled
+                if SchedList[SchedNum-1].LOSt > CompSat.AOSt : #if the previously scheduled sat's end time is greater than the curr sats start time
+                    SchedList.append(CompSat)
+                    SchedNum = SchedNum+1
+            else: #if this is the first sat being scheduled
+                SchedList.append(CompSat)
+                SchedNum = SchedNum+1
+
+    print(SchedList[1].ID)

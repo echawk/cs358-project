56d0df6d24950239df8fd1d129422c0f4caa49f1
ErikjPetersen <65267303+ErikjPetersen@users.noreply.github.com>
2022-04-21 17:27:12 -0500

    added time conversions

--- src/SatellitePass.py
+++ src/SatellitePass.py
@@ -27,24 +27,41 @@ class Pass (object):
 # Functions
 def Schedule(List, StartT, EndT): #function to schedule the given Pass List in the allowed time period
     SchedList = []
-    SchedNum = 0
+    SchedNum = 1
+    numconsidered = 0
     CurrSat = List[0]
+    LST = '0' #Last Scheduled Time
 
-    #THIS IS WHERE THE PRIORITIES OF THE STATION OWNER IS INPUTTED
-    #(this example is only prioritizing success rate)
 
-    for x in range(1,len(List)):
-        CompSat = List[x]
-        if CompSat.Sperc > CurrSat.Sperc:
-            CurrSat = CompSat
+    for x in range(1,len(List)): #Goes through every pass in List
+        CompSat = List[x] #CompSat = relevent pass
+        if StartT <= CompSat[0]: #only enters if start of pass is after the specified start time frame
+            if EndT >= CompSat[1]: #only enters if the end of pass is before the specified end time frame
+                numconsidered = numconsidered+1 #this code runs whenever a sat is being considered
+                if LST < CompSat[0]: #if the last considered time is before the start time
+                    SchedList.append(CompSat)
+                    SchedNum = SchedNum+1
+                    LST = CompSat[1]
 
-    print('After considering', len(List), 'sattelites, scheduling pass of sattelite ID(s): ', CurrSat.ID)
+
+
+
+    print('After considering', numconsidered, 'sattelites, scheduling pass readings of', len(SchedList), 'sattelites in the allocated time period')
+    #print('After considering', len(List), 'sattelites, scheduling pass of sattelite ID(s): ', CurrSat.ID)
     #print('After considering', len(List), 'sattelites, scheduling pass of sattelite ID(s): ', len(SchedList))
-    return
+    return SchedList
 
 def GetPasses(Station, StartT, EndT, minimum_altitude, min_pass_duration):
     a = 0
     return
+
+def string_time_to_unix_time(input_str):
+    ymd_s, hms_s = tuple(input_str.split(" "))
+    year, month, day = tuple(ymd_s.split('-'))
+    hms_s = hms_s.split(".")[0]
+    hour, minute, second = tuple(hms_s.split(':'))
+    date_time = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
+    return ("unix_timestamp => ",(time.mktime(date_time.timetuple())))
 
 
 ################
@@ -58,20 +75,17 @@ sats = sqlite3.connect("C:\\Users\\Yogif\\OneDrive\\Desktop\\Homework\\CS 358\\s
 
 sc = sats.cursor()
 #print(sc.execute('SELECT name FROM sqlite_schema WHERE type=\'table\' AND name NOT LIKE \'sqlite_%\';'))
+PassList = []
+for row in sc.execute('SELECT * FROM passes;'): #fills in the PassList array with all of the 4.8M individual passes
+    PassList.append(row)
 
-for row in sc.execute('SELECT * FROM passes;'):
-    print(row)
+for pass_ in PassList:
+    start, end, duration, rizeaz, setaz, tca, max_el, gs, norad = pass_
 
-#print(df.head())
+#INFO READ IN IS FORMATTED AS: start, end, duration, riseaz, setaz, tca, max el, gs, norad (TCA IRRELEVENT)
 
-S1 = Station(41.462,-87.038,240,834) #VALPO STATION INFO EXAMPLE
-P1 = Pass(99477, 414, 425, 203, 17, 331, 56.48) #example pass from online pass predictor
-P2 = Pass(39430, 415, 428, 31, 14, 145, 63.31) #example pass from online pass predictor
-P3 = Pass(47963, 418, 430, 25, 24, 165, 45.33) #example pass from online pass predictor
-P4 = Pass(28654, 419, 434, 196, 26, 335, 97.77)
-P5 = Pass(47945, 422, 433, 22, 29, 170, 79.06)
-P6 = Pass(43792, 426, 437, 207, 15, 329, 52.11)
-P7 = Pass(43793, 435, 442, 208, 15, 329, 78.49)
-
-PassList = [P1, P2, P3, P4, P5, P6, P7]
-Schedule(PassList, 414, 443)
+InStartTime = '2018-09-12 07:22:40.080409'
+InEndTime = '2018-09-13 21:51:21.865953'
+FinSc = Schedule(PassList, InStartTime, InEndTime)
+for y in range(len(FinSc)):
+    print(FinSc[y])
